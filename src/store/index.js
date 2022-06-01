d58dc80a90900a1a52bcaa81bbbce93c450c8528
Mohamed Taieb Boussoffara <mohamed.taieb.boussoffara@gmail.com>
2022-06-01 14:31:03 +0200

    Persist state on page reload

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import { createStore } from "vuex";
 import modules from "./modules";
+import createPersistedState from "vuex-persistedstate";
+
+const dataState = createPersistedState({
+  key: "todo-state",
+  paths: ["tasks", "theme"],
+});
 
 const store = createStore({
   modules: {
     ...modules,
   },
+  plugins: [dataState],
 });
 
 export default store;
